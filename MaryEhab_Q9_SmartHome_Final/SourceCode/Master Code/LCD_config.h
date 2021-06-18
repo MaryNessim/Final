b30ce8d38ec87fb8f92a81668c6670acfd48bbdc
MaryNessim <81465831+MaryNessim@users.noreply.github.com>
2021-06-18 13:55:01 +0200

    .

--- MaryEhab_Q9_SmartHome_Final/SourceCode/Master Code/LCD_config.h
+++
@@ -1,7 +0,0 @@
-#ifndef LCD_CONFIG_H_
-#define LCD_CONFIG_H_
-
-#define eight_bits_mode
-//#define four_bits_mode
-
-#endif /* LCD_CONFIG_H_ */
--- MaryEhab_Q9_SmartHome_Final/SourceCode/Master Code/bluetooth.h
+++
@@ -1,8 +0,0 @@
-#ifndef BLUETOOTH_H_
-#define BLUETOOTH_H_
-
-extern void bluetooth (uint8 text);
-
-
-
-#endif /* BLUETOOTH_H_ */
